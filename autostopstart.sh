adddb8690cb936ca9ca0656fbc4c6fffe29b2a52
USER <user@Muranushi-MBA.local>
2015-04-23 20:21:46 +0900

    add -> allcount

--- autostopstart.sh
+++ autostopstart.sh
@@ -3,22 +3,23 @@
 Action=$1
 case $Action
    start )
-      Time=`date -v+5M +%H`
+      Time=`date -v-5M +%H`
    ;;
    stop )
-      Time=`date -v-5M +%H`
+      Time=`date -v+5M +%H`
    ;;
 esac
 
-# All region
-Region=`aws ec2 describe-regions |jq -r '.Regions[].RegionName'`
-# All Acount
+# All region & All Acount
+#Region=`aws ec2 describe-regions |jq -r '.Regions[].RegionName'`
 AllAcount=`cat ~/.aws/config |grep "^\[" |sed -e "s/\]//g" -e "s/\[//g"`
 
-
-for Instance in `aws ec2 describe-instances --filter "Name=tag:$Action,Values=$Time" |jq -r '.Reservations[].Instances[].InstanceId'`
+for Acount in ${Acount[@]}
 do
-	aws ec2 $Action-instances --instance-ids $Instance
+   for Instance in `aws ec2 --profile $Acount describe-instances --filter "Name=tag:$Action,Values=$Time" |jq -r '.Reservations[].Instances[].InstanceId'`
+   do
+      aws ec2 $Action-instances --instance-ids $Instance
+   done
 done
 
 exit
